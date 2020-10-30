aa3711238933564830e08983e8e10406a81706cb
Sukhdip <=>
2020-10-30 16:23:43 -0700

    added webview to open wikipedia articles

--- app/src/main/java/com/example/wikipediasummarizer/ui/fragments/WikiArticleFragment.kt
+++ app/src/main/java/com/example/wikipediasummarizer/ui/fragments/WikiArticleFragment.kt
@@ -2,19 +2,33 @@ package com.example.wikipediasummarizer.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.navArgs
 import com.example.wikipediasummarizer.R
+import com.example.wikipediasummarizer.ui.MainActivity
+import com.example.wikipediasummarizer.ui.WikiViewModel
+import com.example.wikipediasummarizer.util.Constants.Companion.WIKI_BASE_URL
 import kotlinx.android.synthetic.main.fragment_wiki_article.*
+import okhttp3.OkHttpClient
 
 class WikiArticleFragment : Fragment(R.layout.fragment_wiki_article) {
 
 
+    lateinit var viewModel: WikiViewModel
     val args: WikiArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        wiki_article_title.text = args.Title
+        viewModel = (activity as MainActivity).viewModel
+
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(WIKI_BASE_URL + args.Title)
+        }
     }
 
 }
--- app/src/main/java/com/example/wikipediasummarizer/util/Constants.kt
+++ app/src/main/java/com/example/wikipediasummarizer/util/Constants.kt
@@ -2,7 +2,8 @@ package com.example.wikipediasummarizer.util
 
 class Constants {
     companion object {
-        const val BASE_URL: String = "https://en.wikipedia.org"
+        const val API_BASE_URL: String = "https://en.wikipedia.org"
+        const val WIKI_BASE_URL: String = "https://en.wikipedia.org/wiki/"
         const val SEARCH_ARTICLE_DELAY = 500L
         const val QUERY_PAGE_SIZE  = 15
         const val THUMBNAIL_WIDTH = 100
